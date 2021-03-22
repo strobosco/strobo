6980513863b2a85b7dd3c9cb9269a147b6b544b2
Niccolo <niccolonobili@gmail.com>
2021-03-22 19:43:21 +0100

    First commit

---
+++ server/src/index.ts
@@ -0,0 +1,36 @@
+import "reflect-metadata";
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+// import { Post } from "./entities/Post";
+import microConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/Post";
+
+const main = async () => {
+  const orm = await MikroORM.init(microConfig);
+
+  await orm.getMigrator().up();
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(8080, () => {
+    console.log("Server started on port 8080");
+  });
+};
+
+main().catch((err) => {
+  console.log(err);
+});
