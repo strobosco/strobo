0d5cf0babc63aea651b046246f75c4ae0550c3d9
Niccolo <niccolonobili@gmail.com>
2021-03-30 23:03:36 +0200

    Added pagination function

--- falco/src/utils/createUrqlClient.ts
+++ falco/src/utils/createUrqlClient.ts
@@ -1,5 +1,10 @@
-import { dedupExchange, fetchExchange, Exchange } from "urql";
-import { cacheExchange } from "@urql/exchange-graphcache";
+import {
+  dedupExchange,
+  fetchExchange,
+  Exchange,
+  stringifyVariables,
+} from "urql";
+import { cacheExchange, Resolver } from "@urql/exchange-graphcache";
 import {
   LogoutMutation,
   MeQuery,
@@ -22,12 +27,121 @@ const errorExchange: Exchange = ({ forward }) => (ops$) => {
   );
 };
 
+// import { stringifyVariables } from "@urql/core";
+// import { Resolver, Variables, NullArray } from "../types";
+
+// export type MergeMode = "before" | "after";
+
+// export interface PaginationParams {
+//   offsetArgument?: string;
+//   limitArgument?: string;
+//   mergeMode?: MergeMode;
+// }
+
+const cursorPagination = (): Resolver => {
+  return (_parent, fieldArgs, cache, info) => {
+    const { parentKey: entityKey, fieldName } = info;
+
+    const allFields = cache.inspectFields(entityKey);
+    const fieldInfos = allFields.filter((info) => info.fieldName === fieldName);
+    const size = fieldInfos.length;
+    if (size === 0) {
+      return undefined;
+    }
+
+    const fieldKey = `${fieldName}(${stringifyVariables(fieldArgs)})`;
+    const isItInCache = cache.resolve(
+      cache.resolve(entityKey, fieldKey) as string,
+      "posts"
+    );
+    info.partial = !isItInCache;
+    const results: string[] = [];
+    let hasMore = true;
+    fieldInfos.forEach((fi) => {
+      // const data = cache.resolve(entityKey, fi.fieldKey) as string[];
+      const key = cache.resolve(entityKey, fi.fieldKey) as string;
+      const data = cache.resolve(key, "posts") as string[];
+      const _hasMore = cache.resolve(key, "hasMore");
+      if (!_hasMore) {
+        hasMore = _hasMore as boolean;
+      }
+      results.push(...data);
+    });
+
+    return {
+      __typename: "PaginatedPosts",
+      hasMore: hasMore,
+      posts: results,
+    };
+
+    //   const visited = new Set();
+    //   let result: NullArray<string> = [];
+    //   let prevOffset: number | null = null;
+
+    //   for (let i = 0; i < size; i++) {
+    //     const { fieldKey, arguments: args } = fieldInfos[i];
+    //     if (args === null || !compareArgs(fieldArgs, args)) {
+    //       continue;
+    //     }
+
+    //     const links = cache.resolve(entityKey, fieldKey) as string[];
+    //     const currentOffset = args[cursorArgument];
+
+    //     if (
+    //       links === null ||
+    //       links.length === 0 ||
+    //       typeof currentOffset !== "number"
+    //     ) {
+    //       continue;
+    //     }
+
+    //     const tempResult: NullArray<string> = [];
+
+    //     for (let j = 0; j < links.length; j++) {
+    //       const link = links[j];
+    //       if (visited.has(link)) continue;
+    //       tempResult.push(link);
+    //       visited.add(link);
+    //     }
+
+    //     if (
+    //       (!prevOffset || currentOffset > prevOffset) ===
+    //       (mergeMode === "after")
+    //     ) {
+    //       result = [...result, ...tempResult];
+    //     } else {
+    //       result = [...tempResult, ...result];
+    //     }
+
+    //     prevOffset = currentOffset;
+    //   }
+
+    //   const hasCurrentPage = cache.resolve(entityKey, fieldName, fieldArgs);
+    //   if (hasCurrentPage) {
+    //     return result;
+    //   } else if (!(info as any).store.schema) {
+    //     return undefined;
+    //   } else {
+    //     info.partial = true;
+    //     return result;
+    //   }
+  };
+};
+
 // create Urql client to use caching
 export const createUrqlClient = (ssrExchange: any) => ({
   url: "http://localhost:8080/graphql",
   exchanges: [
     dedupExchange,
     cacheExchange({
+      keys: {
+        PaginatedPosts: () => null,
+      },
+      resolvers: {
+        Query: {
+          posts: cursorPagination(),
+        },
+      },
       updates: {
         Mutation: {
           logout: (_result, args, cache, info) => {
