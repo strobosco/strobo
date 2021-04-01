eed37e3966a062c19f60a03c9ad0ac70d30531ea
Niccolo <niccolonobili@gmail.com>
2021-04-01 16:02:42 +0200

    Added vote field and column

--- sanso/src/entities/Post.ts
+++ sanso/src/entities/Post.ts
@@ -5,10 +5,12 @@ import {
   CreateDateColumn,
   Entity,
   ManyToOne,
+  OneToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from "typeorm";
 import { User } from "./User";
+import { Vote } from "./Vote";
 
 @ObjectType()
 @Entity()
@@ -36,6 +38,9 @@ export class Post extends BaseEntity {
   @Field()
   @ManyToOne(() => User, (user) => user.posts)
   creator: User;
+
+  @OneToMany(() => Vote, (vote) => vote.post)
+  votes: Vote[];
 
   @Field(() => String)
   @CreateDateColumn()
