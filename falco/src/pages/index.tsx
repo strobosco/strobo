fe78db5cf698b1bad94ea961bd95ab6163e2f2a5
Niccolo <niccolonobili@gmail.com>
2021-04-02 18:46:52 +0200

    Made titles clickable and moved create post

--- falco/src/pages/index.tsx
+++ falco/src/pages/index.tsx
@@ -30,13 +30,6 @@ const Index = () => {
 
   return (
     <Layout variant="regular">
-      <Flex align="center">
-        <Heading>LiReddit</Heading>
-        <NextLink href="/create-post">
-          <Link ml="auto">Create Post</Link>
-        </NextLink>
-      </Flex>
-      <br />
       {!data && fetching ? (
         <div>loading...</div>
       ) : (
@@ -46,7 +39,11 @@ const Index = () => {
             <Flex key={p.id} p={5} shadow="md" borderWidth="1px">
               <VoteSection post={p} />
               <Box>
-                <Heading fontSize="xl">{p.title}</Heading>
+                <NextLink href="/post/[id]" as={`/post/${p.id}`}>
+                  <Link>
+                    <Heading fontSize="xl">{p.title}</Heading>
+                  </Link>
+                </NextLink>
                 <Text>posted by {p.creator.username}</Text>
                 <Text mt={4}>{p.textSnippet + " ..."}</Text>
               </Box>
