bf00dc303057527b9386a5dfd4dd364fb37b77a5
Niccolo <niccolonobili@gmail.com>
2021-04-02 18:46:09 +0200

    Added page to view single post

---
+++ falco/src/pages/post/[id].tsx
@@ -0,0 +1,50 @@
+import { Heading } from "@chakra-ui/layout";
+import { Box } from "@chakra-ui/react";
+import { withUrqlClient } from "next-urql";
+import { useRouter } from "next/router";
+import React from "react";
+import { Layout } from "../../components/Layout";
+import { usePostQuery } from "../../generated/graphql";
+import { createUrqlClient } from "../../utils/createUrqlClient";
+
+const Post = ({}) => {
+  const router = useRouter();
+  const intId =
+    typeof router.query.id === "string" ? parseInt(router.query.id) : -1;
+  const [{ data, error, fetching }] = usePostQuery({
+    pause: intId === -1,
+    variables: {
+      id: intId,
+    },
+  });
+
+  if (fetching) {
+    return (
+      <Layout>
+        <Box>loading...</Box>
+      </Layout>
+    );
+  }
+
+  if (error) {
+    return <Box>{error.message}</Box>;
+  }
+
+  if (!data?.post) {
+    return (
+      <Layout variant="regular">
+        <Box>Post could not be found</Box>
+      </Layout>
+    );
+  }
+
+  // could do without ? since we have a check in place before this
+  return (
+    <Layout variant="regular">
+      <Heading mb={4}>{data?.post?.title}</Heading>
+      {data?.post?.text}
+    </Layout>
+  );
+};
+
+export default withUrqlClient(createUrqlClient, { ssr: true })(Post);
