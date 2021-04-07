308c4187744e773a439960925c726808d2dd0876
Niccolo <niccolonobili@gmail.com>
2021-04-07 22:42:48 +0200

    Added message, following, and explore text

--- falco/src/components/SideBar.tsx
+++ falco/src/components/SideBar.tsx
@@ -6,18 +6,39 @@ interface SideBarProps {}
 
 export const SideBar: React.FC<SideBarProps> = ({}) => {
   return (
-    <Grid position="sticky" templateRows="repeat(2, 1fr)" alignItems="flex-end">
+    <Grid position="sticky" templateRows="repeat(5, 1fr)" alignItems="flex-end">
       <Box>
         <NextLink href="/create-post">
-          <Button as={Link} bg="transparent" fontSize={24} ml={6}>
+          <Button as={Link} bg="transparent" fontSize={32} mt={0} ml={6}>
             Create Post
           </Button>
         </NextLink>
       </Box>
       <Box>
         <NextLink href="/">
-          <Button as={Link} bg="transparent" fontSize={24} ml={6}>
+          <Button as={Link} bg="transparent" fontSize={32} mt={10} ml={6}>
             Home
+          </Button>
+        </NextLink>
+      </Box>
+      <Box>
+        <NextLink href="/">
+          <Button as={Link} bg="transparent" fontSize={32} mt={10} ml={6}>
+            Following
+          </Button>
+        </NextLink>
+      </Box>
+      <Box>
+        <NextLink href="/">
+          <Button as={Link} bg="transparent" fontSize={32} mt={10} ml={6}>
+            Explore
+          </Button>
+        </NextLink>
+      </Box>
+      <Box>
+        <NextLink href="/">
+          <Button as={Link} bg="transparent" fontSize={32} mt={10} ml={6}>
+            Meassages
           </Button>
         </NextLink>
       </Box>
