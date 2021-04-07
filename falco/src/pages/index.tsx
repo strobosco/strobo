2a89f5b05a9c22d41ed94ae9c82308afdd955e25
Niccolo <niccolonobili@gmail.com>
2021-04-07 22:19:17 +0200

    Adde SideBar

--- falco/src/pages/index.tsx
+++ falco/src/pages/index.tsx
@@ -1,20 +1,9 @@
-import {
-  Box,
-  Button,
-  Flex,
-  Heading,
-  Link,
-  Stack,
-  Text,
-} from "@chakra-ui/react";
-import { withUrqlClient } from "next-urql";
-import NextLink from "next/link";
-import React, { useState } from "react";
-import { EditDeletePostButtons } from "../components/EditDeletePostButtons";
+import { Box, Button, Flex } from "@chakra-ui/react";
+import React from "react";
 import { Layout } from "../components/Layout";
-import { VoteSection } from "../components/VoteSection";
-import { PostsQuery, useMeQuery, usePostsQuery } from "../generated/graphql";
-import { createUrqlClient } from "../utils/createUrqlClient";
+import { PostsView } from "../components/PostsView";
+import { SideBar } from "../components/SideBar";
+import { usePostsQuery } from "../generated/graphql";
 import { withApollo } from "../utils/withApollo";
 
 const Index = () => {
@@ -32,39 +21,14 @@ const Index = () => {
 
   return (
     <Layout variant="regular">
-      {!data && loading ? (
-        <div>loading...</div>
-      ) : (
-        <Stack spacing={8}>
-          // add format text output
-          {data!.posts.posts.map((p) =>
-            !p ? null : (
-              <Flex key={p.id} p={5} shadow="md" borderWidth="1px">
-                <VoteSection post={p} />
-                <Box flex={1}>
-                  <NextLink href="/post/[id]" as={`/post/${p.id}`}>
-                    <Link>
-                      <Heading fontSize="xl">{p.title}</Heading>
-                    </Link>
-                  </NextLink>
-                  <Text>posted by {p.creator.username}</Text>
-                  <Flex align="center">
-                    <Text flex={1} mt={4}>
-                      {p.textSnippet + " ..."}
-                    </Text>
-                    <Box ml="auto">
-                      <EditDeletePostButtons
-                        id={p.id}
-                        creatorId={p.creator.id}
-                      />
-                    </Box>
-                  </Flex>
-                </Box>
-              </Flex>
-            )
-          )}
-        </Stack>
-      )}
+      <Flex>
+        <Box w="20%" ml={4} mr="auto">
+          <SideBar />
+        </Box>
+        <Box w="60%" mr="auto">
+          {!data && loading ? <div>loading...</div> : <PostsView data={data} />}
+        </Box>
+      </Flex>
       {data && data.posts.hasMore ? (
         <Flex>
           <Button
@@ -75,25 +39,6 @@ const Index = () => {
                   cursor:
                     data.posts.posts[data.posts.posts.length - 1].createdAt,
                 },
-                // updateQuery: (
-                //   previousValue,
-                //   { fetchMoreResult }
-                // ): PostsQuery => {
-                //   if (!fetchMoreResult) {
-                //     return previousValue as PostsQuery;
-                //   }
-                //   return {
-                //     __typename: "Query",
-                //     posts: {
-                //       __typename: "PaginatedPosts",
-                //       hasMore: (fetchMoreResult as PostsQuery).posts.hasMore,
-                //       posts: [
-                //         ...(previousValue as PostsQuery).posts.posts,
-                //         ...(fetchMoreResult as PostsQuery).posts.posts,
-                //       ],
-                //     },
-                //   };
-                // },
               });
             }}
             isLoading={loading}
