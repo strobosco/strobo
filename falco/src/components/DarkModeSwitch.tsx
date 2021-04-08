eb6f816970237140cd1bc83641b6ad5808ccd89a
Niccolo <niccolonobili@gmail.com>
2021-04-08 21:47:27 +0200

    Added dark mode toggle

--- falco/src/components/DarkModeSwitch.tsx
+++ falco/src/components/DarkModeSwitch.tsx
@@ -1,16 +1,14 @@
-import { useColorMode, Switch } from '@chakra-ui/react'
+import { useColorMode, IconButton } from "@chakra-ui/react";
+import { SunIcon, MoonIcon } from "@chakra-ui/icons";
 
 export const DarkModeSwitch = () => {
-  const { colorMode, toggleColorMode } = useColorMode()
-  const isDark = colorMode === 'dark'
+  const { colorMode, toggleColorMode } = useColorMode();
+  const isDark = colorMode === "dark";
   return (
-    <Switch
-      position="fixed"
-      top="1rem"
-      right="1rem"
-      color="green"
-      isChecked={isDark}
-      onChange={toggleColorMode}
+    <IconButton
+      aria-label="Toggle dark mode"
+      icon={isDark ? <SunIcon /> : <MoonIcon />}
+      onClick={toggleColorMode}
     />
-  )
-}
+  );
+};
