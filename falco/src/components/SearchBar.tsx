877fc25a420a65ac572211d2ff8693dc5b670f50
Niccolo <niccolonobili@gmail.com>
2021-04-11 17:35:16 +0200

    Added search functionality

--- falco/src/components/SearchBar.tsx
+++ falco/src/components/SearchBar.tsx
@@ -1,10 +1,16 @@
-import { Box } from "@chakra-ui/react";
+import { SearchIcon } from "@chakra-ui/icons";
+import { Box, Button, Flex, Heading, IconButton } from "@chakra-ui/react";
 import { Field, Form, Formik } from "formik";
 import React from "react";
+import { usePostsSearchLazyQuery } from "../generated/graphql";
+import { useRouter } from "next/router";
 
 interface SearchBarProps {}
 
 export const SearchBar: React.FC<SearchBarProps> = ({}) => {
+  const [PostsSearch, { data }] = usePostsSearchLazyQuery();
+  const router = useRouter();
+
   const myStyle = {
     color: "black",
     width: "100%",
@@ -17,22 +23,41 @@ export const SearchBar: React.FC<SearchBarProps> = ({}) => {
     borderColor: "rgb(169,169,169)",
   };
   return (
-    <Box>
+    <Box alignItems="center">
       <Formik
         initialValues={{ search: "" }}
-        onSubmit={async () => {
-          return;
+        onSubmit={async (values) => {
+          await PostsSearch({
+            variables: { filter: values.search },
+          });
+          if (data?.postsSearch) {
+            console.log(data.postsSearch[0]);
+            router.push(`/post/${data.postsSearch[0].id}`);
+          }
         }}
       >
         <Form>
-          <Field
-            as="input"
-            name="search"
-            placeholder="..."
-            style={myStyle}
-          ></Field>
+          <Flex>
+            <Field
+              as="input"
+              name="search"
+              placeholder="..."
+              style={myStyle}
+            ></Field>
+            <IconButton
+              aria-label="Search box"
+              icon={<SearchIcon />}
+              colorScheme="teal"
+              type="submit"
+              ml={2}
+            />
+            <Button type="submit">Submit</Button>
+          </Flex>
         </Form>
       </Formik>
+      {/* {data?.postsSearch?.map((p) => {
+        console.log(p);
+      })} */}
     </Box>
   );
 };
